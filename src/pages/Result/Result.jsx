970cb94f9e36219736dcbbf761eb218bcfc94911
Pranav Rao <pranavrao541@gmail.com>
2024-04-05 16:45:57 +0530

    feat: poll-result

--- src/pages/Result/Result.jsx
+++ src/pages/Result/Result.jsx
@@ -1,31 +1,112 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Button from "../../components/Button/Button";
 import { useEffect, useState } from "react";
 import { firestore } from "../../firebase/firebase";
-import { doc, getDoc } from "firebase/firestore";
+import { collection, doc, onSnapshot, query, where } from "firebase/firestore";
 
 const Result = () => {
   const { pollId } = useParams();
   const [poll, setPoll] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [userVote, setUserVote] = useState(null);
 
   useEffect(() => {
     const fetchPoll = async () => {
       const pollRef = doc(firestore, "polls", pollId);
-      const pollDoc = await getDoc(pollRef);
 
-      if (pollDoc.exists()) {
-        setPoll({ id: pollDoc.id, ...pollDoc.data() });
-        setLoading(false);
-      } else {
-        console.log("No Such Poll Created");
-      }
+      // Subscribe to real-time updates on the poll document
+      const unsubscribe = onSnapshot(pollRef, (doc) => {
+        if (doc.exists()) {
+          setPoll({ id: doc.id, ...doc.data() });
+          setLoading(false);
+        } else {
+          console.log("No Such Poll Created");
+        }
+      });
+
+      // Cleanup function to unsubscribe from real-time updates when the component unmounts
+      return () => unsubscribe();
     };
 
     fetchPoll();
   }, [pollId]);
 
-  const handleVoting = () => {};
+  useEffect(() => {
+    const fetchUserVote = async () => {
+      const currentUserData = JSON.parse(
+        localStorage.getItem("currentUserData")
+      );
+      const userId = currentUserData?.uid;
+
+      const votesQuery = query(
+        collection(firestore, "votes"),
+        where("pollId", "==", pollId),
+        where("userId", "==", userId)
+      );
+
+      const unsubscribe = onSnapshot(votesQuery, (snapshot) => {
+        if (!snapshot.empty) {
+          const userVote = snapshot.docs[0].data().optionText;
+          setPoll((prevState) => ({
+            ...prevState,
+            userVote,
+          }));
+          setUserVote(userVote);
+        }
+      });
+
+      return () => unsubscribe();
+    };
+
+    fetchUserVote();
+  }, [pollId]);
+
+  /**Function to count votes for each option
+   * @returns count of votes every poll option has
+   */
+  const fetchVoteCounts = async () => {
+    // Map to keep track of listeners
+    const listeners = new Map();
+
+    // Function to set up a listener for a specific option
+    const setupListener = (option) => {
+      const votesQuery = query(
+        collection(firestore, "votes"),
+        where("pollId", "==", pollId),
+        where("optionText", "==", option)
+      );
+
+      const unsubscribe = onSnapshot(votesQuery, (snapshot) => {
+        const voteCount = snapshot.size;
+        setPoll((prevState) => ({
+          ...prevState,
+          voteCounts: {
+            ...prevState.voteCounts,
+            [option]: voteCount,
+          },
+        }));
+      });
+
+      // Store the unsubscribe function in the map
+      listeners.set(option, unsubscribe);
+    };
+
+    // Set up listeners for all options
+    for (const option of poll?.options || []) {
+      setupListener(option);
+    }
+
+    // Cleanup function to unsubscribe from all listeners
+    return () => {
+      listeners.forEach((unsubscribe) => unsubscribe());
+    };
+  };
+
+  useEffect(() => {
+    if (!loading && poll) {
+      fetchVoteCounts();
+    }
+  }, [loading, poll]);
 
   return (
     <section className="w-full min-h-[90vh] p-3 flex flex-col items-center justify-center gap-5">
@@ -36,45 +117,79 @@ const Result = () => {
           <h1 className="text-[35px] font-black text-gray-900 md:text-[40px] lg:text-[50px]">
             Poll Result
           </h1>
-          <div className="w-full flex items-center justify-center flex-col gap-3">
-            <div className="w-full h-auto p-3 rounded-md border-2 border-gray-900 bg-gray-600 text-gray-300 lg:w-1/3 md:w-1/2 flex flex-col gap-3">
-              <h2 className="w-full text-md lg:text-xl md:text-lg bg-gray-900 py-2 px-2 rounded-md tracking-wide">
-                Voting for Poll-Id:{" "}
-                <span className="text-[#0088FF]">{pollId}</span>
-              </h2>
-              <h2 className="w-full text-md lg:text-md md:text-lg bg-gray-900 py-2 px-2 rounded-md tracking-wide">
-                <div>
-                  <h2 className="w-full text-md lg:text-xl md:text-lg bg-gray-600 rounded-md tracking-wide py-1.5 px-1.5">
-                    Poll Question:{" "}
-                    <span className="text-[#0088FF]">{poll?.question}</span>
+          <div className="h-auto flex flex-col flex-wrap items-center justify-center md:flex-row lg:flex-row gap-5">
+            <div className="flex items-center flex-wrap justify-center flex-col gap-5 lg:gap-5">
+              <div className="shadow-md shadow-[#00000093] w-full md:w-full lg:w-full text-md lg:text-md md:text-lg bg-gray-600 py-2 px-2 rounded-md tracking-wide border-2 border-gray-900 lg:p-5 flex flex-col gap-1">
+                <div className="bg-gray-900 w-full text-md lg:text-xl md:text-lg rounded-md tracking-wide py-1.5 px-1.5">
+                  <h2 className="text-[white] font-bold">
+                    Poll Question:
+                    <span className="text-[#0088FF] text-[17px]">
+                      {poll?.question}
+                    </span>
                   </h2>
                 </div>
-                <div className="mt-2.5 px-1.5">
+                <div className="mt-2.5 px-1.5 text-[silver] font-bold">
                   Poll Created By:{" "}
-                  <span className="text-[#0088FF]">{poll?.createdBy}</span>
+                  <span className="text-[#0088FF] font-normal">
+                    {poll?.createdBy}
+                  </span>
                 </div>
-                <div className="px-1.5">
+                <div className="mb-3 px-1.5 text-[silver] font-bold">
                   Poll Created At:{" "}
-                  <span className="text-[#0088FF] text-sm">
+                  <span className="text-[#0088FF] font-normal text-sm">
                     {poll?.createdAt?.toDate().toLocaleString()}
                   </span>
                 </div>
-              </h2>
-            </div>
-            <div className="w-full h-auto p-3 rounded-md border-2 border-gray-900 bg-gray-700 text-gray-300 lg:w-1/3 md:w-1/2">
-              <h1 className="w-full text-2xl mb-3 md:text-3xl">Poll Options</h1>
-              <div className="flex flex-col items-start justify-start gap-3">
-                {poll?.options.map((option, index) => (
-                  <h1
-                    key={index}
+                <div>
+                  <Link
+                    to={`/vote/${pollId}`}
                     className={
-                      "py-2 px-5 bg-gray-800 rounded-md hover:bg-white hover:text-gray-900 transition-all duration-200 tracking-wide"
+                      "px-5 py-2 cursor-pointer rounded-md bg-[#0088FF] hover:bg-[#2B00FF] text-white text-[17px] transition-all duration-200 focus:bg-[#2B00FF] focus:outline-none"
                     }
-                    value={option}
+                    value={"Poll Results"}
                   >
-                    {option}
-                  </h1>
-                ))}
+                    Poll Voting
+                  </Link>
+                </div>
+              </div>
+              <div className="shadow-md shadow-[#00000093] w-full md:w-full lg:w-full text-md lg:text-md md:text-lg bg-gray-900 py-2 px-2 rounded-md tracking-wide border-2 border-gray-900 lg:p-5 text-white">
+                <h1 className="w-full text-2xl mb-3 md:text-2xl bg-gray-600 rounded-md p-1.5">
+                  Poll Options
+                </h1>
+                <div className="flex flex-col items-start justify-start gap-3">
+                  {poll?.options.map((option, index) => (
+                    <h1
+                      key={index}
+                      className={`py-2 px-5 ${
+                        userVote === option
+                          ? "bg-white text-gray-900"
+                          : "bg-gray-600"
+                      } rounded-md hover:bg-white hover:text-gray-600 transition-all duration-200 tracking-wide`}
+                      value={option}
+                    >
+                      {userVote === option && "Your Vote 👉 "}
+                      {option}
+                    </h1>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center flex-wrap justify-center flex-col gap-5 lg:gap-5">
+              <div className="shadow-md shadow-[#00000093] w-full md:w-full lg:w-full text-md lg:text-md md:text-lg bg-gray-900 py-2 px-2 rounded-md tracking-wide border-2 border-gray-900 lg:p-5 text-white">
+                <h1 className="w-full text-2xl mb-3 md:text-2xl bg-gray-600 rounded-md p-1.5">
+                  Total Votes
+                </h1>
+                <div className="flex flex-col items-start justify-start gap-3">
+                  {poll?.options.map((option, index) => (
+                    <h1
+                      key={index}
+                      className={`py-2 px-5 rounded-md hover:bg-white hover:text-gray-600 transition-all duration-200 tracking-wide`}
+                      value={option}
+                    >
+                      {option} [{poll.voteCounts?.[option] || 0}]
+                    </h1>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
