d2172104e61f15c3588ef680fc4278d3c234a735
Pranav Rao <pranavrao541@gmail.com>
2024-04-07 00:39:06 +0530

    firebase: firebase-config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
+  apiKey: "AIzaSyB7BMR9U5xOKsOXh95Rh3A1KqYgTi2reOs",
+  authDomain: "polling-website-1493c.firebaseapp.com",
+  projectId: "polling-website-1493c",
+  storageBucket: "polling-website-1493c.appspot.com",
+  messagingSenderId: "217148584136",
+  appId: "1:217148584136:web:523f5da4dfbd2c79d1b5fd",
 };
 
 const app = initializeApp(firebaseConfig);
